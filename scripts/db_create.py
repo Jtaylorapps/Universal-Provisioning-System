fee2bd7bf5dc5973b663ac28984f7fb7451d94df
Jake Taylor <jtaylorapps@gmail.com>
2016-11-07 15:54:24 -0500

    Removed extra images

--- scripts/db_create.py
+++ scripts/db_create.py
@@ -1,11 +1,9 @@
 from migrate.versioning import api
-from sqlalchemy import types
-from sqlalchemy.ext.compiler import compiles
-
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
 from app import db
 import os.path
+
 db.drop_all()
 db.create_all()
 if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
